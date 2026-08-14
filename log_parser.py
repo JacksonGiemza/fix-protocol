91affcb50d30980f70b9f4f27896d39859bef8d3
Jackson Giemza <jackson.giemza@gmail.com>
2026-08-14 18:21:43 -0500

    basic parser, added a mock log file, and fix repo data and xlm parser for that

---
+++ log_parser.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import json
+
+df = pd.DataFrame(columns=["head","body","tail"])
+
+with open("fix_log1.txt", "r", encoding="utf-8") as file:
+    log = file.read()
+
+log = log.split('')
+
+curr = ''
+i = -1
+
+data = {}
+for element in log:
+    element = element.replace("\n", "")
+    if element == '':
+        continue
+
+    if element[:2] == "8=":
+        i += 1
+        curr = 'head'
+
+    if element[:3] == "35=":
+        curr = 'body'
+
+    if element[:3] == "10=":
+        curr = 'tail'
+
+    if i not in data:
+        data[i] = {'head': [], 'body': [], 'tail': []}
+
+    data[i][curr].append(element)
+
+with open(r"data\log.json", "w") as file:
+        json.dump(data, file)
---
+++ xml_parser.py
@@ -0,0 +1,47 @@
+import xml.etree.ElementTree as ET
+import json
+
+def fieldsToDict(root):
+    fields = {}
+    for field in root.findall('Field'):
+        tag_id = field.find('Tag').text
+
+        tag_data = {
+        }
+        for elem in field.iter():
+            if elem.tag == 'Tag' or elem.tag == 'Field':
+                continue
+            tag_data[elem.tag] = elem.text
+        fields[tag_id] = tag_data
+
+    return fields
+
+def enumsToDict(root):
+    enums = {}
+    for enum in root.findall('Enum'):
+        key = enum.find('Tag').text
+
+        if key not in enums:
+            enums[key] = {enum.find('Value').text: enum.find('SymbolicName').text}
+        else:
+            enums[key][enum.find('Value').text] = enum.find('SymbolicName').text
+    return enums
+
+def main():
+    fields_path = r"fix_repository\FIX.4.4\Base\Fields.xml"
+    enums_path = r"fix_repository\FIX.4.4\Base\Enums.xml"
+    fields = ET.parse(fields_path)
+    enums = ET.parse(enums_path)
+
+    fields_data = fieldsToDict(fields)
+    enums_data = enumsToDict(enums)
+
+    with open(r"data\fix_fields.json", "w") as file:
+        json.dump(fields_data, file)
+
+    with open(r"data\fix_enums.json", "w") as file:
+        json.dump(enums_data, file)
+
+if __name__ == "__main__":
+    main()
+
